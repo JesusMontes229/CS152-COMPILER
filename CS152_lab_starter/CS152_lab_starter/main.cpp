d915d83a63c8a34a32729a96ad344bce96e89666
JesusMontes229 <jmmontes217@gmail.com>
2025-10-05 22:03:26 -0700

    hello world is active

--- CS152_lab_starter/CS152_lab_starter/main.cpp
+++ CS152_lab_starter/CS152_lab_starter/main.cpp
@@ -9,15 +9,36 @@ int main() {
     string wasm =R"(
 (module
   (import "env" "print" (func $print (param i32)))
+  (import "env" "putch" (func $putch (param i32)))
   
   (func $main (result i32)
-    i32.const 42
-    call $print
-    i32.const 0)
-    
-  (export "main" (func $main))
+    i32.const 72
+    call $putch
+    i32.const 101
+    call $putch
+    i32.const 108
+    call $putch
+    i32.const 108
+    call $putch
+    i32.const 111
+    call $putch
+    i32.const 32
+    call $putch
+    i32.const 87
+    call $putch
+    i32.const 111
+    call $putch
+    i32.const 114
+    call $putch
+    i32.const 108
+    call $putch
+    i32.const 100
+    call $putch
+
+    i32.const 0
 )
-    )";
+  (export "main" (func $main))
+))";
     cout << WEB_PAGE_PREAMBLE;
     cout << wasm << "\n";
     cout << WEB_PAGE_POSTAMBLE;    
